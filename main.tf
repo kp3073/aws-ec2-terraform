0223cb0d5ec088824f269fce3292fbf563d6c9d0
kp3073 <keyurpatel8288@gmail.com>
2024-07-17 11:00:46 -0400

    1

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 module "ec2" {
   source = "./modules/ec2"
   instance_type = var.instance_type
-  env = lab
 }
--- vars.tf
+++ vars.tf
@@ -1,2 +1,4 @@
-variable "instance_type" {}
+variable "instance_type" {
+  default = "t3.micro"
+}
 variable "env" {}
